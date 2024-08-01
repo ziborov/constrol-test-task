a3363c6be72e603536022039e792c677643cc4fd
Alexandr Ziborov <alexandr@getta.io>
2024-08-01 19:47:19 +0300

    Fix getHello

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import {View} from "react-native";
+import React from "react";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World555!';
+    return `<View>
+
+           Hello 
+           
+           World555566677788889999!
+
+        </View>`;
   }
 }
